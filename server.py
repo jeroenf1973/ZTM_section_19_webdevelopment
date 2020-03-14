c939eb7397f5eb41d344bb0a5a025d06534f9174
JeFi <jeroenf1973@hotmail.com>
2020-03-14 22:27:35 +0100

    contact form data in csv file

--- server.py
+++ server.py
@@ -5,6 +5,7 @@
 # server vanuit (venv) opstarten met flask run
 
 from flask import Flask, render_template, url_for, request
+import csv
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,7 +23,7 @@ def submit_form():
     if request.method == "POST":
         data = request.form.to_dict()
         #print(data)
-        write_to_file(data)
+        write_to_csv(data)
         return render_template('thankyou.html')
     else:
         return "oops....thats not good!"
@@ -34,6 +35,16 @@ def write_to_file(data):
         message = data["message"]
         file  = database.write(f"\n{email},{subject},{message}")
         # schrijf values naar file
+
+def write_to_csv(data):
+    with open('database.csv', newline='',  mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer  = csv.writer(database2, delimiter=",",  quotechar='"', quoting =csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+        # schrijf values naar file
+
 
 
 # @app.route('/index.html')
