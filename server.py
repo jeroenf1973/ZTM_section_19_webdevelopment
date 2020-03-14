49e0cdf637e22dd07aac0b50cd4cf35d7eea051a
JeFi <jeroenf1973@hotmail.com>
2020-03-14 22:04:26 +0100

    contact form data to file and response

--- server.py
+++ server.py
@@ -4,33 +4,60 @@
 # met alleen set commando kan je checken of deze gezet is
 # server vanuit (venv) opstarten met flask run
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    #return 'Hello World; Running a Flask server on my laptop!'
-    # render template gebruit standaard template folder
-    print(url_for('static', filename='favicon.ico'))
+def my_home():
     return render_template('index.html')
 
-# @app.route('/favicon.ico')
-# def favicon:
-#     return ''
+# Algemene route
+@app.route('/<string:page_name>')
+def htmlpage(page_name=None):
+    return render_template(page_name)
 
-@app.route('/users/<username>/<int:post_id>')
-def username(username=None, post_id=None):
-    return render_template('about.html', name_placeholder=username, post_id=post_id)
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    #return 'form submitted!'
+    if request.method == "POST":
+        data = request.form.to_dict()
+        #print(data)
+        write_to_file(data)
+        return render_template('thankyou.html')
+    else:
+        return "oops....thats not good!"
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file  = database.write(f"\n{email},{subject},{message}")
+        # schrijf values naar file
 
-@app.route('/blog')
-def blog():
-    return 'Hier zouden blogs kunnen komen..'
+
+# @app.route('/index.html')
+# def my_index():
+#     return render_template('index.html')
+
+# @app.route('/works.html')
+# def works():
+#     return render_template('works.html')
+
+# @app.route('/work.html')
+# def work():
+#     return render_template('work.html')
+
+# # @app.route('/about.html')
+# # def about():
+# #     return render_template('about.html')
+
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
+
+# @app.route('/components.html')
+# def components():
+#     return render_template('components.html')
 
 
-@app.route('/blog/2020/dogs')
-def blog2():
-    return 'woef!'
